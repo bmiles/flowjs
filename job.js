953b227894ea78f883815ebe6da331630556bd94
Ben Miles <ben.miles2@gmail.com>
2014-09-21 23:43:08 +0100

    added a git ignore

--- job.js
+++ job.js
@@ -1,26 +1,9 @@
 var flow = require('./flowjs');
 
-var ben = new flow.SLI1000('ben',0);
+var sli1000 = new flow.SLI1000('sli1000',0);
 
-sli1000.on("ready", function() {
+sli1000.getSensorStatus();
 
-
-});
-serialPort.on('open', function () {
-  console.log('open');
-  serialPort.on('data', function(data) {
-    console.log('data received: ' + data);
-    console.log(typeof(data));
-    console.log('got %d bytes of data', data.length);
-    console.log(data);
-    var buffData = readContent(flow.stripStartStop(data))
-  });
-  //device info
-  serialPort.write([0x7E, 0x00, 0xD0, 0x01, 0x01, 0x2D, 0x7E], function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results.toString());
-    console.log(results);
-  });
-  //get single measurement
-
-});
+sli1000.startSingleMeasurement();
+var measurement = sli1000.getSingleMeasurement();
+console.log(measurement);
