340206fdff20841eb20b631b35f29bcee5311c32
Ben Miles <ben.miles2@gmail.com>
2014-09-22 16:42:32 +0100

    made a lot of progress example actually outputs flow rate

--- flowjs.js
+++ flowjs.js
@@ -13,37 +13,54 @@ var ld = require('lodash');
 // the state, usually an error code, the number of data containing bytes, and
 // finally the data itself.
 
-var readContent = function(buf) {
-  var buf = buf.slice(1,buf.length - 1);
-  var bufferJSON = {
-    'devAddress' : buf.readUInt8(0),
-    'command' : buf.readUInt8(1),
-    'state' : buf.readUInt8(2),
-    'chksum' : buf.readUInt8(buf.length),
-    'dataLength' : buf.readUInt8(3),
-    'dataContent' : buf.slice(4,buf.length - 1)
-  };
-  return bufferJSON;
+var readResponse = function(buf) {
+  console.log(buf.toString());
+  if (buf.toString().match(/~.*~/g)) {
+    console.log(buf);
+    var bufferJSON = {
+      'devAddress' : buf.readUInt8(1),
+      'command' : buf.readUInt8(2),
+      'state' : buf.readUInt8(3),
+      'chksum' : buf.readUInt8(buf.length -2),
+      'dataLength' : buf.readUInt8(4),
+      //'responseData' : buf.readUInt8(5)
+    };
+    console.log(buf);
+    if (bufferJSON.dataLength > 0) {
+      bufferJSON.responseData = buf.slice(5,buf.length - 2);
+    } else {
+      bufferJSON.responseData = undefined;
+    }
+    return bufferJSON;
+  } else {
+    console.log('serial read failed');
+    return undefined; }
 };
 
-// the read data function acceps the dataContent buffer from the readcontent function.
+// the read data function acceps the responseData buffer from the readResponse function.
 var readData = function(dataBuf) {
-  var scaleFactor = 13;
-  var sensorOutput = (dataBuf.readUInt8(0) << 8) + dataBuf.readUInt8(1);
-  // measurementDataType refers to if the sensor output is signed or unsigned.
-  if (measurementDataType === 0) {
-    // had to add in this l variable as doing the this in the if statement didn't work.
-    var l = sensorOutput & 32768;
-    if (l === 32768) {
-      var flowTicks = -((sensorOutput ^ 65535) +1);
+  if (dataBuf === undefined) {
+    console.log('no data');
+    return undefined;
+  } else {
+    var scaleFactor = 13;
+    var sensorOutput = (dataBuf.readUInt8(0) << 8) + dataBuf.readUInt8(1);
+    // measurementDataType refers to if the sensor output is signed or unsigned.
+    if (0 === 0) {
+      // had to add in this l variable as doing the this in the if statement didn't work.
+      var l = sensorOutput & 32768;
+      if (l === 32768) {
+        var flowTicks = -((sensorOutput ^ 65535) +1);
+      } else {
+        var flowTicks = sensorOutput;
+      }
     } else {
       var flowTicks = sensorOutput;
-    }
-  } else {
-    var flowTicks = sensorOutput;
-    }
-  var physicalFlow = flowTicks / scaleFactor;
-  return physicalFlow;
+      }
+    var physicalFlow = flowTicks / scaleFactor;
+    console.log(physicalFlow + 'uL/min');
+    return physicalFlow;
+  };
 };
 
 // calculates the chksum for the byte array without stop and start
@@ -63,6 +80,34 @@ var addStartStop = function (bArr) {
   return bArr;
 };
 
+// Error State Handling
+var errorHandler = function (state) {
+  switch(state) {
+    case 0x00:
+        return console.log('No Error');
+    case 0x01:
+        return console.log('RS485 Error: Wrong Data Size');
+    case 0x02:
+        return console.log('RS485 Error: Unknown command');
+    case 0x03:
+        return console.log('RS485 Error: No access rights for command');
+    case 0x04:
+        return console.log('RS485 Error: Invalid parameter');
+    case 0x20:
+        return console.log('Sensor Error: Sensor Busy');
+    case 0x21:
+        return console.log('Sensor Error: No Ack from Sensor');
+    case 0x22:
+        return console.log('Sensor Error: I2C CRC false');
+    case 0x23:
+        return console.log('Sensor Error: Sensor timeout');
+    case 0x24:
+        return console.log('Sensor Error: No measurement started');
+    default:
+        return console.log('No Error');
+  }
+};
+
 // var stuffIt = function(bArr) {
 //   // reg ex for 0x7e
 //   // stuff bytes to replace it
@@ -79,18 +124,40 @@ function SLI1000(name, address) {
   }, function(err) {
       if (err) {console.log(err);}
   });
+  var device = this;
+  // one open port per device.
+  device.serialPort.on('open', function () {
+    console.log('open');
+    device.serialPort.flush(function(error) {
+      device.serialPort.on('data', function(data) {
+        console.log(data);
+        var content = readResponse(data);
+        console.log(content);
+        if (content.state === 0x00) {
+          if (content.command === 0x32) {
+            return readData(content.responseData);
+          } else {
+            console.log('not sensor measurment');
+          }//device.serialPort.close;
+        } else {
+          return console.log(errorHandler(content.state));
+        }
+      });
+    });
+  });
 }
 
 SLI1000.prototype.getSensorStatus = function() {
+  var device = this;
   var command = 0x30;
   var address = this.address;
   var byteArr = [address, command];
   addChkSum(byteArr);
   addStartStop(byteArr);
 
-  this.serialPort.on('open', function () {
+  device.serialPort.on('open', function () {
     console.log('open');
-    this.serialPort.on('data', function(data) {
+    device.this.serialPort.on('data', function(data) {
       console.log('data received: ' + data);
       console.log(typeof(data));
       console.log('got %d bytes of data', data.length);
@@ -99,58 +166,79 @@ SLI1000.prototype.getSensorStatus = function() {
       console.log(bufstring);
     });
     //device info
-    this.serialPort.write(byteArr, function(err, results) {
+    device.serialPort.write(byteArr, function(err, results) {
       console.log('err ' + err);
       console.log('results ' + results.toString());
       console.log(results);
+      device.serialPort.drain(function(error) {return;});
     });
   });
 };
 
-SLI1000.prototype.startSingleMeasurement = function() {
+SLI1000.prototype.startSingleMeasurement = function(callback) {
+  var device = this;
   var command = 0x31;
-  var address = this.address;
-  var byteArr = [address, command];
+  var address = device.address;
+  var byteArr = [address, command, 0x00];
   addChkSum(byteArr);
   addStartStop(byteArr);
   // Port open
-  this.serialPort.on('open', function () {
-    console.log('open');
-    this.serialPort.on('data', function(err,data) {
-      if (!err) {
-        var content = readContent(data);
-        if (content.state === 0x00) {
-          return readData(content.dataContent);
-        } else {
-          return err;
-        }
-      } else {
-        console.log(err);
-        }
-    });
+  // device.serialPort.on('open', function () {
+  //   console.log('open');
+  //   device.serialPort.on('data', function(data) {
+  //     var content = readResponse(data);
+  //     console.log(content);
+  //     if (content.state === 0x00) {
+  //       console.log('Single measurement started...');
+  //       //return callback;
+  //     } else {
+  //       return console.log(errorHandler(content.state));
+  //     }
+  //   });
     // Dispatch and read or err
-    this.serialPort.write(byteArr, function(err, results) {
+    device.serialPort.write(byteArr, function(err, results) {
       if (!err) {
-        var content = readContent(results);
-        if (content.state === 0x00) {
-          return readData(content.dataContent);
-        } else {
-          return err;
-        }
+        device.serialPort.drain(function(error) {
+          return console.log('Sent: ' + byteArr);
+        });
       } else {
-        console.log(err);
+        return console.log(err);
         }
       });
-  });
+//  });
 };
 
 SLI1000.prototype.getSingleMeasurement = function() {
-  var command = 0x32;
-  var address = this.address;
-  var byteArr = [address, command];
-  addChkSum(byteArr);
-  addStartStop(byteArr);
-  //dispatch;
+var device = this;
+var command = 0x32;
+var address = device.address;
+var byteArr = [address, command, 0x00];
+addChkSum(byteArr);
+addStartStop(byteArr);
+// Port open
+// device.serialPort.on('open', function () {
+//   console.log('open');
+//   device.serialPort.on('data', function(data) {
+//     var content = readResponse(data);
+//     console.log(content);
+//     if (content.state === 0x00) {
+//       //device.serialPort.close;
+//       return readData(content.responseData);
+//     } else {
+//       return console.log(errorHandler(content.state));
+//     }
+//   });
+  // Dispatch and read or err
+  device.serialPort.write(byteArr, function(err, results) {
+    if (!err) {
+      device.serialPort.drain(function(error) {
+        return console.log('Sent: ' + byteArr);
+      });
+    } else {
+      return console.log(err);
+      }
+    });
+//});
 };
 
 
--- job.js
+++ job.js
@@ -1,9 +1,17 @@
+'use strict';
+
 var flow = require('./flowjs');
 
 var sli1000 = new flow.SLI1000('sli1000',0);
 
-sli1000.getSensorStatus();
+//sli1000.getSensorStatus();
+setInterval(function(){
+  sli1000.startSingleMeasurement();
+  setTimeout(function(){
+    console.log('getting...');
+    sli1000.getSingleMeasurement();
+  }, 1000);
+}, 3000);
 
-sli1000.startSingleMeasurement();
-var measurement = sli1000.getSingleMeasurement();
-console.log(measurement);
+
+//var measurement = sli1000.getSingleMeasurement();
